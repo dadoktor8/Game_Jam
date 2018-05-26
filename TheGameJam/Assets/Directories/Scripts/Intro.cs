6f5c891c0b4f4b8e8d930b4d471b8f10c0273add
Kostas Man <anthosnet@hotmail.com>
2018-05-26 17:18:06 +0300

    Intro Scene
    Intro Scene, changed font in Game Menu

---
+++ TheGameJam/Assets/Directories/Scripts/Intro.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Intro : MonoBehaviour {
+
+    [SerializeField] Image Fading;
+    [SerializeField] Animator anim;
+    [SerializeField] float IntroTime = 10.0f;
+    [SerializeField] int Index = 2;
+    [SerializeField] AudioSource IntroMusic;
+
+    double musicVolume = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+
+        StartCoroutine(FadeTo() );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator FadeTo()
+    {
+        yield return new WaitForSeconds(IntroTime);
+        anim.SetBool("Fade", true);
+        StartCoroutine(StartLevel() );
+        
+    }
+
+    IEnumerator StartLevel()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(Index);
+    }
+}
